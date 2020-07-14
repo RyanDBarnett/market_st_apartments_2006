b5a7d91af1f228c1dfe9aeb1f9c66ad63d9929b0
RyanDBarnett <conner2468@gmail.com>
2020-07-14 09:28:53 -0600

    Add renter attribute to apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,10 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
   def initialize(apartment_data)
     @number = apartment_data[:number]
     @monthly_rent = apartment_data[:monthly_rent]
     @bathrooms = apartment_data[:bathrooms]
     @bedrooms = apartment_data[:bedrooms]
+    @renter = nil
   end
 end
