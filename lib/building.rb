4ade2a073983e07c2c62a4e686aff6ad8b9fee06
RyanDBarnett <conner2468@gmail.com>
2020-07-14 10:00:31 -0600

    Add renters method

--- lib/building.rb
+++ lib/building.rb
@@ -1,16 +1,20 @@
 class Building
-  attr_reader :units, :renters
+  attr_reader :units
   def initialize
     @units = []
-    @renters = []
   end
 
   def add_unit unit
     @units << unit
   end
 
-  def add_renter renter
-    @renters << renter
+  def renters
+    @units.reduce([]) do |renters, unit|
+      if unit.renter
+        renters << unit.renter
+      end
+      renters
+    end
   end
 
   def average_rent
