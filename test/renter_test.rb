2efbeaf09f50620c7fb0a492e049b25063653917
RyanDBarnett <conner2468@gmail.com>
2020-07-14 09:12:51 -0600

    Add different name test to renter

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -4,14 +4,19 @@ require './lib/renter'
 
 class RenterTest < Minitest::Test
   def setup
-    @renter = Renter.new("Ryan")
+    @renter1 = Renter.new("Ryan")
+    @renter2 = Renter.new("John")
   end
 
   def test_it_exists
-    assert_instance_of Renter, @renter
+    assert_instance_of Renter, @renter1
   end
 
   def test_it_has_a_name
-    assert_equal "Ryan", @renter.name
+    assert_equal "Ryan", @renter1.name
+  end
+
+  def test_it_can_have_a_different_name
+    assert_equal "John", @renter2.name
   end
 end
