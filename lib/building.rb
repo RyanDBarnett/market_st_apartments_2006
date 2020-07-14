86dbf6d519bdaffc970a381630632065bc6222e2
RyanDBarnett <conner2468@gmail.com>
2020-07-14 10:28:54 -0600

    Create renter with highest rent method

--- lib/building.rb
+++ lib/building.rb
@@ -9,14 +9,22 @@ class Building
   end
 
   def renters
-    occupied_units.map { |unit| unit.renter }
+    rented_units.map { |unit| unit.renter }
   end
 
   def average_rent
-    occupied_units.reduce(0) { |sum, unit| sum += unit.monthly_rent } / occupied_units.length
+    rented_units.reduce(0) { |sum, unit| sum += unit.monthly_rent } / rented_units.length
   end
 
   def rented_units
     @units.select { |unit| unit.renter }
+  end
+
+  def renter_with_highest_rent
+    if rented_units.length > 0
+      rented_units.sort_by { |unit| unit.monthly_rent }.reverse[0].renter
+    else
+      nil
+    end
   end
 end
