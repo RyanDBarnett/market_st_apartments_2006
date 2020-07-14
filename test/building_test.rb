2392dcb74d899f1263c9dff3b923aa5867e65524
RyanDBarnett <conner2468@gmail.com>
2020-07-14 10:07:45 -0600

    Refactor average rent test and add occupied units test

--- test/building_test.rb
+++ test/building_test.rb
@@ -38,20 +38,51 @@ class BuildingTest < Minitest::Test
     assert_equal [], @building.renters
 
     unit.add_renter(renter)
-    
+
     assert_equal [renter], @building.renters
   end
 
-  def test_average_rent
+  def test_occupied_units
+    renter1 = Renter.new("Ryan")
+    renter2 = Renter.new("Bob")
     apartment_data1 = {number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1}
     unit1 = Apartment.new(apartment_data1)
     apartment_data2 = {number: "A2", monthly_rent: 1800, bathrooms: 2, bedrooms: 2}
     unit2 = Apartment.new(apartment_data2)
-    expected = (1200 + 1800) / 2
 
     @building.add_unit(unit1)
     @building.add_unit(unit2)
 
-    assert_equal expected, @building.average_rent
+    assert_equal [], @building.occupied_units
+
+    unit1.add_renter(renter1)
+
+    assert_equal [unit1], @building.occupied_units
+
+    unit2.add_renter(renter2)
+
+    assert_equal [unit1, unit2], @building.occupied_units
+  end
+
+  def test_average_rent
+    renter1 = Renter.new("Ryan")
+    renter2 = Renter.new("Bob")
+    apartment_data1 = {number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1}
+    unit1 = Apartment.new(apartment_data1)
+    apartment_data2 = {number: "A2", monthly_rent: 1800, bathrooms: 2, bedrooms: 2}
+    unit2 = Apartment.new(apartment_data2)
+
+    @building.add_unit(unit1)
+    @building.add_unit(unit2)
+
+    assert_equal [], @building.renters
+
+    unit1.add_renter(renter1)
+
+    assert_equal 1200, @building.average_rent
+
+    unit2.add_renter(renter2)
+
+    assert_equal 1500, @building.average_rent
   end
 end
