a374d4872aaec645abd02ef9407e31f6abc799e9
RyanDBarnett <conner2468@gmail.com>
2020-07-14 09:41:15 -0600

    Create add unit building test

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,10 +7,6 @@ require './lib/building'
 class BuildingTest < Minitest::Test
   def setup
     @building = Building.new
-    apartment_data1 = {number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1}
-    apartment_data2 = {number: "A2", monthly_rent: 1800, bathrooms: 2, bedrooms: 2}
-    @unit1 = Apartment.new(apartment_data1)
-    @unit2 = Apartment.new(apartment_data2)
   end
 
   def test_it_exists
@@ -19,5 +15,13 @@ class BuildingTest < Minitest::Test
 
   def test_it_has_units_that_defaults_to_empty_array
     assert_equal [], @building.units
+  end
+
+  def test_can_add_unit
+    apartment_data1 = {number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1}
+    unit1 = Apartment.new(apartment_data1)
+
+    @building.add_unit(unit1)
+    assert_equal [unit1], @building.units
   end
 end
