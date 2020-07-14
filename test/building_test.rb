033e9c5b6e9685b185ae46781eff0fcc2068716d
RyanDBarnett <conner2468@gmail.com>
2020-07-14 09:36:57 -0600

    Add building exists test

---
+++ test/building_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+
+class BuildingTest < Minitest::Test
+  def setup
+    @building = Building.new
+    apartment_data1 = {number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1}
+    apartment_data2 = {number: "A2", monthly_rent: 1800, bathrooms: 2, bedrooms: 2}
+    @unit1 = Apartment.new(apartment_data1)
+    @unit2 = Apartment.new(apartment_data2)
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building
+  end
+end
