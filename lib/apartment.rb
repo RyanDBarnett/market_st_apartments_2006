8ddb4879fb71d9acf6835f27b860d922bfc7860b
RyanDBarnett <conner2468@gmail.com>
2020-07-14 09:23:29 -0600

    Add apartment monthly rent attribute

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,6 +1,7 @@
 class Apartment
-  attr_reader :number
+  attr_reader :number, :monthly_rent
   def initialize(apartment_data)
     @number = apartment_data[:number]
+    @monthly_rent = apartment_data[:monthly_rent]
   end
 end
